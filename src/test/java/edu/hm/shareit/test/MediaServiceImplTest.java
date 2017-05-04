48783ca9e999b438fe969b6d500795177840dc07
Michael Eggers <eggers@hm.edu>
2017-05-04 22:20:33 +0200

    uncommented tests

--- src/test/java/edu/hm/shareit/test/MediaServiceImplTest.java
+++ src/test/java/edu/hm/shareit/test/MediaServiceImplTest.java
@@ -40,116 +40,116 @@ public class MediaServiceImplTest {
 
     }
 
-//    @Test
-//    public void bookTest() {
-//        // add book
-//        result = service.addBook(testB1);
-//        assertEquals(MediaServiceResult.OK, result);
-//        result = service.addBook(testB2);
-//        assertEquals(MediaServiceResult.OK, result);
-//
-//        // isbn reserved
-//        result = service.addBook(testB1);
-//        assertEquals(MediaServiceResult.ISBN_RESERVED, result);
-//        // invalid isbn
-//        result = service.addBook(new Book(title, author, invalidISBN));
-//        assertEquals(MediaServiceResult.INVALID_ISBN, result);
-//        // missing info
-//        result = service.addBook(new Book("", author, isbn));
-//        assertEquals(MediaServiceResult.MISSING_INFO, result);
-//        result = service.addBook(new Book(title, "", isbn));
-//        assertEquals(MediaServiceResult.MISSING_INFO, result);
-//        result = service.addBook(null);
-//        assertEquals(MediaServiceResult.IM_A_TEAPOT, result);
-//
-//        // get books
-//        Book[] testBooks = new Book[2];
-//        testBooks[0] = testB1;
-//        testBooks[1] = testB2;
-//        Book[] books = (Book[]) service.getBooks();
-//
-//        assertArrayEquals(books, testBooks);
-//
-//        // update book
-//        result = service.updateBook(new Book("", "", testB1.getIsbn()));
-//        assertEquals(MediaServiceResult.OK, result);
-//
-//        Book update = new Book(title2, author2, testB2.getIsbn());
-//        result = service.updateBook(update);
-//        assertEquals(MediaServiceResult.OK, result);
-//        // ok
-//        Optional<Medium> test = service.getBook(testB2.getIsbn());
-//        assertTrue(test.isPresent());
-//        Book bookUpdated = (Book) test.get();
-//        assertEquals(bookUpdated.getAuthor(), author2);
-//        assertEquals(bookUpdated.getTitle(), title2);
-//        // not found
-//        result = service.updateBook(new Book("xxx", "xxx", "978-3-16-148410-4"));
-//        assertEquals(MediaServiceResult.NOT_FOUND, result);
-//
-//        // get books
-//
-//        testBooks[1] = update;
-//        books = (Book[]) service.getBooks();
-//        assertArrayEquals(books, testBooks);
-//
-//        Optional<Medium> updated = service.getBook("154685312154");
-//        assertFalse(updated.isPresent());
-//
-//    }
-//
-//    @Test
-//    public void discTest() {
-//        MediaServiceResult result = MediaServiceResult.IM_A_TEAPOT;
-//        Disc disc1 = new Disc(barcode, director, fsk, title3);
-//        Disc disc2 = new Disc(barcode2, director, fsk, title3);
-//
-//        // ok
-//        result = service.addDisc(disc1);
-//        assertEquals(MediaServiceResult.OK, result);
-//        result = service.addDisc(disc2);
-//        assertEquals(MediaServiceResult.OK, result);
-//        // barcode reserved
-//        result = service.addDisc(disc1);
-//        assertEquals(MediaServiceResult.BARCODE_RESERVED, result);
-//
-//        // missing barcode
-//        result = service.addDisc(new Disc("", director, fsk, title3));
-//        assertEquals(MediaServiceResult.MISSING_BARCODE, result);
-//
-//        // TODO invalid barcode
-//        // result = service.addDisc(new Disc("2",director,fsk,title3));
-//        // assertEquals(MediaServiceResult.INVALID_BARCODE,result);
-//        // missing info
-//        result = service.addDisc(new Disc(barcode, "", fsk, title3));
-//        assertEquals(MediaServiceResult.MISSING_INFO, result);
-//        // get discs
-//        Disc[] discs = (Disc[]) service.getDiscs();
-//        Disc[] testDiscs = new Disc[2];
-//        testDiscs[0] = disc1;
-//        testDiscs[1] = disc2;
-//        assertArrayEquals(testDiscs, discs);
-//        // update discs
-//        final int fsk2 = 12;
-//        result = service.updateDisc(new Disc(barcode, "", fsk2, ""));
-//        assertEquals(MediaServiceResult.OK, result);
-//        result = service.updateDisc(new Disc(barcode, "", -1, "The Guardians of the Galaxy 2"));
-//        assertEquals(MediaServiceResult.OK, result);
-//        result = service.updateDisc(new Disc(barcode, "Rocket", -1, ""));
-//        assertEquals(MediaServiceResult.OK, result);
-//        result = service.updateDisc(disc1);
-//        assertEquals(MediaServiceResult.OK, result);
-//
-//        result = service.updateDisc(new Disc("00333", "", fsk2, ""));
-//        assertEquals(MediaServiceResult.NOT_FOUND, result);
-//
-//        // get discs
-//        Optional<Medium> updated = service.getDisc(barcode);
-//        assertTrue(updated.isPresent());
-//        assertEquals(updated.get(), disc1);
-//        updated = service.getDisc("5548814");
-//        assertFalse(updated.isPresent());
-//
-//    }
+    @Test
+    public void bookTest() {
+        // add book
+        result = service.addBook(testB1);
+        assertEquals(MediaServiceResult.OK, result);
+        result = service.addBook(testB2);
+        assertEquals(MediaServiceResult.OK, result);
+
+        // isbn reserved
+        result = service.addBook(testB1);
+        assertEquals(MediaServiceResult.ISBN_RESERVED, result);
+        // invalid isbn
+        result = service.addBook(new Book(title, author, invalidISBN));
+        assertEquals(MediaServiceResult.INVALID_ISBN, result);
+        // missing info
+        result = service.addBook(new Book("", author, isbn));
+        assertEquals(MediaServiceResult.MISSING_INFO, result);
+        result = service.addBook(new Book(title, "", isbn));
+        assertEquals(MediaServiceResult.MISSING_INFO, result);
+        result = service.addBook(null);
+        assertEquals(MediaServiceResult.IM_A_TEAPOT, result);
+
+        // get books
+        Book[] testBooks = new Book[2];
+        testBooks[0] = testB1;
+        testBooks[1] = testB2;
+        Book[] books = (Book[]) service.getBooks();
+
+        assertArrayEquals(books, testBooks);
+
+        // update book
+        result = service.updateBook(new Book("", "", testB1.getIsbn()));
+        assertEquals(MediaServiceResult.OK, result);
+
+        Book update = new Book(title2, author2, testB2.getIsbn());
+        result = service.updateBook(update);
+        assertEquals(MediaServiceResult.OK, result);
+        // ok
+        Optional<Medium> test = service.getBook(testB2.getIsbn());
+        assertTrue(test.isPresent());
+        Book bookUpdated = (Book) test.get();
+        assertEquals(bookUpdated.getAuthor(), author2);
+        assertEquals(bookUpdated.getTitle(), title2);
+        // not found
+        result = service.updateBook(new Book("xxx", "xxx", "978-3-16-148410-4"));
+        assertEquals(MediaServiceResult.NOT_FOUND, result);
+
+        // get books
+
+        testBooks[1] = update;
+        books = (Book[]) service.getBooks();
+        assertArrayEquals(books, testBooks);
+
+        Optional<Medium> updated = service.getBook("154685312154");
+        assertFalse(updated.isPresent());
+
+    }
+
+    @Test
+    public void discTest() {
+        MediaServiceResult result = MediaServiceResult.IM_A_TEAPOT;
+        Disc disc1 = new Disc(barcode, director, fsk, title3);
+        Disc disc2 = new Disc(barcode2, director, fsk, title3);
+
+        // ok
+        result = service.addDisc(disc1);
+        assertEquals(MediaServiceResult.OK, result);
+        result = service.addDisc(disc2);
+        assertEquals(MediaServiceResult.OK, result);
+        // barcode reserved
+        result = service.addDisc(disc1);
+        assertEquals(MediaServiceResult.BARCODE_RESERVED, result);
+
+        // missing barcode
+        result = service.addDisc(new Disc("", director, fsk, title3));
+        assertEquals(MediaServiceResult.MISSING_BARCODE, result);
+
+        // TODO invalid barcode
+        // result = service.addDisc(new Disc("2",director,fsk,title3));
+        // assertEquals(MediaServiceResult.INVALID_BARCODE,result);
+        // missing info
+        result = service.addDisc(new Disc(barcode, "", fsk, title3));
+        assertEquals(MediaServiceResult.MISSING_INFO, result);
+        // get discs
+        Disc[] discs = (Disc[]) service.getDiscs();
+        Disc[] testDiscs = new Disc[2];
+        testDiscs[0] = disc1;
+        testDiscs[1] = disc2;
+        assertArrayEquals(testDiscs, discs);
+        // update discs
+        final int fsk2 = 12;
+        result = service.updateDisc(new Disc(barcode, "", fsk2, ""));
+        assertEquals(MediaServiceResult.OK, result);
+        result = service.updateDisc(new Disc(barcode, "", -1, "The Guardians of the Galaxy 2"));
+        assertEquals(MediaServiceResult.OK, result);
+        result = service.updateDisc(new Disc(barcode, "Rocket", -1, ""));
+        assertEquals(MediaServiceResult.OK, result);
+        result = service.updateDisc(disc1);
+        assertEquals(MediaServiceResult.OK, result);
+
+        result = service.updateDisc(new Disc("00333", "", fsk2, ""));
+        assertEquals(MediaServiceResult.NOT_FOUND, result);
+
+        // get discs
+        Optional<Medium> updated = service.getDisc(barcode);
+        assertTrue(updated.isPresent());
+        assertEquals(updated.get(), disc1);
+        updated = service.getDisc("5548814");
+        assertFalse(updated.isPresent());
+
+    }
 
 }
